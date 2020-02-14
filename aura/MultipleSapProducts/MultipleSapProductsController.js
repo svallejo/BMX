4d8c4617efef6ab4fae0dd1ae23ee6be16858d1c
Shaun Vallejo <shaun.vallejo@spartasystems.com>
2020-02-14 17:53:17 +0000

    CI deployment from job `BMX Code Tracking`

--- aura/MultipleSapProducts/MultipleSapProductsController.js
+++ aura/MultipleSapProducts/MultipleSapProductsController.js
@@ -112,5 +112,44 @@
 
         // Send action off to be executed
         $A.enqueueAction(action);
-     }
+     },
+     deleteRecord:function(component, event, helper){
+        var action = component.get("c.removeRecords");
+        var selectedRows = component.get('v.selectedRows');
+        console.log('Deleting selected rows: ' + selectedRows);
+        action.setParams({'sapRecords' : selectedRows});
+        // Add callback behavior for when response is received
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                let result = response.getReturnValue();
+                console.log(JSON.stringify(response.getReturnValue()))
+                component.set('v.selectedRows', []);
+
+                $A.get('e.force:refreshView').fire();  
+            }
+            else {
+                let errors = response.getError();
+                let message = 'Unknown error'; // Default error message
+                // Retrieve the error message sent by the server
+                if (errors && Array.isArray(errors) && errors.length > 0) {
+                    message = errors[0].message;
+                }
+                // Display the message
+                console.error(message)
+                helper.handleErrors("Error", errors, "error");
+            }
+        });
+
+        // Send action off to be executed
+        $A.enqueueAction(action);
+     },
+     setSelectedProduct:function(component, event, helper){
+        let lines = [];
+        lines = component.find('prodTable').getSelectedRows();
+        var showDelete = lines.length >= 1 ? true : false;
+
+        component.set("v.showDelete", showDelete);
+        component.set("v.selectedRows", lines);
+    }
 })
