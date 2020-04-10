76fb3a8024ee9daa7d2866411ae6f3258d845dd2
Shaun Vallejo <shaun.vallejo@spartasystems.com>
2020-04-10 17:54:42 +0000

    CI deployment from job `BMX Code Tracking`

--- aura/MultipleSapProducts/MultipleSapProductsController.js
+++ aura/MultipleSapProducts/MultipleSapProductsController.js
@@ -153,7 +153,31 @@
         component.set("v.showDelete", showDelete);
         component.set("v.selectedRows", lines);
     },
-        recordUpdated:function(cmp, event, helper){
-            console.log('record updated')
+    recordUpdated: function(component, event, helper) {
+        var eventParams = event.getParams();
+        if(eventParams.changeType === "CHANGED") {
+            // get the fields that are changed for this record
+            var changedFields = eventParams.changedFields;
+            console.log('Fields that are changed: ' + JSON.stringify(changedFields));
+            // record is changed so refresh the component (or other component logic)
+            var resultsToast = $A.get("e.force:showToast");
+            resultsToast.setParams({
+                "title": "Saved",
+                "message": "The record was updated."
+            });
+            resultsToast.fire();
+        } else if(eventParams.changeType === "LOADED") {
+            console.log("Record is loaded successfully.");
+        } else if(eventParams.changeType === "REMOVED") {
+            var resultsToast = $A.get("e.force:showToast");
+            resultsToast.setParams({
+                "title": "Deleted",
+                "message": "The record was deleted."
+            });
+            resultsToast.fire();
+        } else if(eventParams.changeType === "ERROR") {
+            console.log('Error: ' + component.get("v.error"));
         }
+    }
+
 })
