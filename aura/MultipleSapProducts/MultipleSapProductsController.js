d86c8f5293e63465c6b2c56723c6bae54ad50145
Shaun Vallejo <shaun.vallejo@spartasystems.com>
2020-05-25 17:55:10 +0000

    CI deployment from job `BMX Code Tracking`

--- aura/MultipleSapProducts/MultipleSapProductsController.js
+++ aura/MultipleSapProducts/MultipleSapProductsController.js
@@ -152,6 +152,61 @@
 
         component.set("v.showDelete", showDelete);
         component.set("v.selectedRows", lines);
-    }
+    },
+    refresh:function(cmp, event, helper){
+        var ccAction = cmp.get("c.areMultipleProductsImpacted");
+        var action = cmp.get("c.retrieveSapProductInformation");
+        action.setParams({'recordId' : cmp.get("v.recordId")});
+        ccAction.setParams({'recordId' : cmp.get("v.recordId")});
+        ccAction.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                let result = response.getReturnValue();
+                // console.log('response: ' + response.getReturnValue())
+                // console.log(JSON.stringify(response.getReturnValue()))
+                cmp.set("v.areMultipleProductsImpacted", result);
+            }
+            else {
+                let errors = response.getError();
+                let message = 'Unknown error'; // Default error message
+                // Retrieve the error message sent by the server
+                if (errors && Array.isArray(errors) && errors.length > 0) {
+                    message = errors[0].message;
+                }
+                // Display the message
+                console.error(message)
+                // helper.handleErrors("Error", errors, "error");
+            }
+            cmp.set("v.HideSpinner", false);
+        });
+
+        // Send action off to be executed
+        $A.enqueueAction(ccAction);
+
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                let result = response.getReturnValue();
+                console.log('response: ' + response.getReturnValue())
+                console.log(JSON.stringify(response.getReturnValue()))
+                cmp.set("v.data", result);
+            }
+            else {
+                let errors = response.getError();
+                let message = 'Unknown error'; // Default error message
+                // Retrieve the error message sent by the server
+                if (errors && Array.isArray(errors) && errors.length > 0) {
+                    message = errors[0].message;
+                }
+                // Display the message
+                console.error(message)
+                // helper.handleErrors("Error", errors, "error");
+            }
+            cmp.set("v.HideSpinner", false);
+        });
+
+        // Send action off to be executed
+        $A.enqueueAction(action);
+     }
 
 })
